0613b212f268365f57b060af798915465f4ab069
g123udini <g123udini@gmail.com>
2022-11-14 11:57:21 +0300

    need to make search

--- views/task/list.php
+++ views/task/list.php
@@ -5,26 +5,6 @@ use yii\grid\GridView;
 use yii\helpers\Html; ?>
 <main class="content__main">
     <h2 class="content__main-heading">Список задач</h2>
-
-    <form class="search-form" action="index.php" method="GET" autocomplete="off">
-        <input class="search-form__input" type="text" name="search" value="" placeholder="Поиск по задачам">
-
-        <input class="search-form__submit" type="submit" name="" value="Искать">
-    </form>
-
-    <div class="tasks-controls">
-        <nav class="tasks-switch">
-            <a href="index.php" class="tasks-switch__item tasks-switch__item--active">Все задачи</a>
-            <a href="index.php?filter=today" class="tasks-switch__item tasks-switch__item--active">Повестка дня</a>
-            <a href="index.php?filter=tomorrow" class="tasks-switch__item tasks-switch__item--active">Завтра</a>
-            <a href="index.php?filter=overdue" class="tasks-switch__item tasks-switch__item--active">Просроченные</a>
-        </nav>
-
-        <label class="checkbox">
-            <input class="checkbox__input visually-hidden show_completed" type="checkbox" checked>
-            <span class="checkbox__text">Показывать выполненные</span>
-        </label>
-    </div>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'tableOptions' => [
